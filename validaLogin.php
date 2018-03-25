f77e15728f2e418e5c9c6cb82cbfbe0dbecafac9
Vitor Ebertz <ebertzv@outlook.com>
2018-03-25 18:12:01 -0300

    PKSoft v0.00001 (Only a login screen)

--- validaLogin.php
+++ validaLogin.php
@@ -1,37 +1,20 @@
 <?php
 	include("connectDB.php");
 	
-
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 		$myusername = $_POST['username'];
 		$mypassword = $_POST['password'];
 
-		$sql = mysqli_query($conexao,"SELECT nameUser, passwordUser FROM users WHERE nameUser = '$myusername' AND passwordUser = '$mypassword'") or die("Erro");
+		$sql = mysqli_query($conexao,"SELECT nameUser, passwordUser FROM users WHERE nameUser = '$myusername' AND passwordUser = '$mypassword'") or die("Erro na consulta ao banco de dados!");
 		$dados = mysqli_fetch_assoc($sql);
 
 		if($dados['nameUser'] == $myusername && $dados['passwordUser'] == $mypassword){
 			session_start();
 			$_SESSION['sess_user'] = $myusername;
 			header("Location: ../PKSoft/Index.html");
+		} else{
+			header("Location: ../PKSoft/Login.html");
 		}
-
-		
-		#$result = mysqli_execute($sql);
-		#$numrows = mysqli_num_rows($result);
-
-		#if($numrows==1){
-		#	while($row=mysql_fetch_assoc($sql)){
-		#		$dbusername=$row['nameUser'];  
-    	#		$dbpassword=$row['passwordUser'];
-    	#		
-    	#		session_register($myusername);
-		#		$_SESSION['login_user'] = $myusername;
-#
-		#		header("location: Index.html");  
-		#	}
-		#} else {
-		#	echo "Usuario ou senha invalidos!";
-		#}
 	}
 ?>
