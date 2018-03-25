8edfcbdf5db54caaa48fdb90b332501a882ce3f9
Vitor Ebertz <ebertzv@outlook.com>
2018-03-24 22:10:04 -0300

    PKSoft v0.0001 (login screen)

--- validaLogin.php
+++ validaLogin.php
@@ -1,18 +1,19 @@
 <?php
-	include("../Back-end/ctrl/connectDB.php");
+	include("connectDB.php");
 	
 
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
+
 		$myusername = $_POST['username'];
 		$mypassword = $_POST['password'];
 
-		$sql = mysqli_query($conexao,"SELECT nameUser, passwordUser FROM user WHERE nameUser = '$myusername' AND passwordUser = '$mypassword'") or die("Erro");
+		$sql = mysqli_query($conexao,"SELECT nameUser, passwordUser FROM users WHERE nameUser = '$myusername' AND passwordUser = '$mypassword'") or die("Erro");
 		$dados = mysqli_fetch_assoc($sql);
 
 		if($dados['nameUser'] == $myusername && $dados['passwordUser'] == $mypassword){
 			session_start();
 			$_SESSION['sess_user'] = $myusername;
-			header("Location: ../Front-end/Index.html");
+			header("Location: ../PKSoft/Index.html");
 		}
 
 		
