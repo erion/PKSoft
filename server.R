a840faf7346d2a918cb05b094426dc1200fd831a
EbertzVitor <vitorebertz@gmail.com>
2018-04-26 21:58:22 -0300

    V 0.5 - Get & post adicionados

--- server.R
+++ server.R
@@ -1,45 +1,39 @@
 # server.R
 
-#library(plumber)
-#library(jsonlite)
-#library(df2json)
-#library(RMySQL)
-#r <- plumb("server.R")
-#r$run(port=8000)
-
-#GET:
-#tabela pacientes, tabela tratamentos (por paciente), tabela histórico (por tratamento), tabela fármacos
-#cada função do get tem uma função de acesso ao banco relacionada
-#pesquisa_pacientes, pesquisa_tratamentos, pesquisa_historico
-#dados_paciente para retornar as infos daquele paciente
-#avançado: simulação
-
-#POST:
-#novo paciente
-#update paciente
-#remover paciente
-#novo tratamento
-#update tratamento
-#remover tratamento
-#novo dado histórico
-#remover dado histórico
-
-#agora no início eu insiro todas as funções do GET
-
 #* @filter cors
 cors <- function(res) {
   res$setHeader("Access-Control-Allow-Origin", "*")
   plumber::forward()
 }
 
-#* @post /testePost
-testePost <- function(teste){
-  fileConn<-file("output.txt")
-  writeLines(teste, fileConn)
-  close(fileConn)
-  return("Ma oe")
+#* @post /novoPaciente
+novoPaciente <- function(paciente){
+  df_paciente <- json2df(paciente)
+  conexao <- abre_conexao()
+  query <- paste("INSERT INTO paciente(nome_paciente,cpf_paciente,nascimento_paciente,genero_paciente,peso_paciente,altura_paciente,cr_paciente,unid_int_paciente,observacao_paciente,rg_paciente,telefone_paciente,agente_saude) VALUES('",df_paciente[1],"',",df_paciente[2],",'",df_paciente[3],"',",df_paciente[4],",",df_paciente[5],",",df_paciente[6],",",df_paciente[7],",'",df_paciente[8],"','",df_paciente[9],"',",df_paciente[10],",",df_paciente[11],",'",df_paciente[12],"')")
+  dbSendQuery(conexao,query)
+  dbDisconnect(conexao)
+  return("Paciente inserido com sucesso!")
 }
 
+#* @post /novoTratamento
+novoTratamento <- function(tratamento){
+  df_tratamento <- json2df(tratamento)
+  conexao <- abre_conexao()
+  query <- paste("INSERT INTO tratamento(cod_paciente, cod_farmaco) VALUES(",df_tratamento[1],",",df_tratamento[2],")")
+  dbSendQuery(conexao,query)
+  dbDisconnect(conexao)
+  return("Tratamento inserido com sucesso!")
+}
+
+#* @post /novoEvento
+novoEvento <- function(evento){
+  df_evento <- json2df(evento)
+  conexao <- abre_conexao()
+  query <- paste("INSERT INTO historico(atributo_historico, valor_historico, data_hora_historico, cod_tratamento) VALUES('",df_evento[1],"',",df_evento[2],",'",df_evento[3],"',",df_evento[4],")")
+  dbSendQuery(conexao,query)
+  disconnect(conexao)
+}
 
 #* @get /tabela_pacientes
 get_tabela_pacientes <- function(){
@@ -74,12 +68,25 @@ get_dados_historico <- function(cod_tratamento){
   return(jsondf)
 }
 
+#* @get /get_farmacos
+get_farmacos <- function(){
+  return(df2json(tabela_farmacos()))
+}
+
 abre_conexao <- function(){
   conexao <- dbConnect(MySQL(),user="0130841",password="zforzachariah",dbname="farmacocinetica", host="ceted.feevale.br", port=3306)
   return(conexao)
 }
 
 #todas as funções com acesso a banco de dados para retorno do GET
+
+tabela_farmacos <- function(){
+  query_farmaco <- paste("SELECT cod_farmaco, nome_farmaco FROM farmaco")
+  rsFarmaco <- dbSendQuery(conexao,query_farmaco)
+  data_farmaco <- fetch(rsFarmaco, n=10)
+  df_farmaco <- data.frame(data_farmaco)
+  return(df_farmaco)
+}
 
 tabela_pacientes <- function(){
   abriu_conexao <- abre_conexao()
